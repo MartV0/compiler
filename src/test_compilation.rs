9cd743ba6d439fdd72a9922520d58f5064e7b5a8
Martijn Voordouw <martijnvoordouw@gmail.com>
2026-03-22 10:26:38 +0100

    Random name for temporary test executable
    Needed because file busy errors

--- src/test_compilation.rs
+++ src/test_compilation.rs
@@ -1,15 +1,16 @@
 #![cfg(test)]
-use std::path::Path;
+use std::path::{Path, PathBuf};
 use std::process::{Command, Output};
 use std::fs;
 use std::os::unix::fs::PermissionsExt;
+use rand::distr::{Alphanumeric, SampleString};
 
 use crate::assembling::assemble;
 use crate::compiling::CompilationResult;
 use crate::linking;
 
-pub fn test_full_compiler(program: &str) -> Result<Output, std::io::Error> {
-    let temp_file = Path::new("/tmp/test_program");
+fn test_full_compiler(program: &str) -> Result<Output, std::io::Error> {
+    let temp_file = &random_file_name();
     crate::compile(program, temp_file);
     test_executable(temp_file)
 }
@@ -17,16 +18,23 @@ pub fn test_full_compiler(program: &str) -> Result<Output, std::io::Error> {
 pub fn test_assembler(input: CompilationResult) -> Result<Output, std::io::Error> {
     let assembled = assemble(input);
     let linked = linking::elf::create_elf(assembled);
-    let temp_file = Path::new("/tmp/test_program");
+    let temp_file = &random_file_name();
     fs::write(temp_file, linked).expect("failed to write to file");
     test_executable(temp_file)
 }
 
-pub fn test_executable(path: &Path) -> Result<Output, std::io::Error> {
+fn test_executable(path: &Path) -> Result<Output, std::io::Error> {
     let executable = PermissionsExt::from_mode(0o700);
     fs::set_permissions(path, executable)?;
     Command::new(path)
         .output()
+}
+
+fn random_file_name() -> PathBuf {
+    let mut path = "/tmp/test_program".to_string();
+    let mut rng = rand::rng();
+    Alphanumeric.append_string(&mut rng, &mut path, 20);
+    PathBuf::from(path)
 }
 
 #[cfg(test)]
